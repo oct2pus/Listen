c2dfa33d0101fabbd9e59869fdc10321dba731d5
Octopus Octopus <oct2pus@jade.moe>
2019-03-02 17:40:44 -0600

    removed unused func call

--- gui/widgets/trackbox/trackbox.go
+++ gui/widgets/trackbox/trackbox.go
@@ -6,9 +6,6 @@ import (
 
 // SetValues handles all widget values that belong to GUI.TrackBox.
 func SetValues(w gui.GUI) gui.GUI {
-	// ImgTrack
-	w.ImgTrack.SetFromPixbuf(setSize(w.ImgTrack.GetPixbuf()))
-
 	// TrackBox
 	w.TrackBox.Add(w.ImgTrack)
 	w.TrackBox.ShowAll()
