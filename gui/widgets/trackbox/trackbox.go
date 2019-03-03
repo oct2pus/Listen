38b9b9dfe48d21f79e1e7ca7967f6412358ceb1e
Octopus Octopus <oct2pus@jade.moe>
2019-03-03 00:52:28 -0600

    center trackbox

--- gui/widgets/trackbox/trackbox.go
+++ gui/widgets/trackbox/trackbox.go
@@ -1,6 +1,7 @@
 package trackbox
 
 import (
+	"github.com/gotk3/gotk3/gtk"
 	"listen/gui"
 )
 
@@ -8,6 +9,9 @@ import (
 func SetValues(w gui.GUI) gui.GUI {
 	// TrackBox
 	w.TrackBox.Add(w.ImgTrack)
+	w.ImgTrack.SetHExpand(true)
+	w.ImgTrack.SetVExpand(true)
+	w.ImgTrack.SetHAlign(gtk.ALIGN_CENTER)
 	w.TrackBox.ShowAll()
 
 	return w
