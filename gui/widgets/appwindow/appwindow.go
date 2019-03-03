177ca37099617e6710a8cf2b7dc7f1505c7c22f5
Octopus Octopus <oct2pus@jade.moe>
2019-03-03 00:10:14 -0600

    set default size

--- gui/widgets/appwindow/appwindow.go
+++ gui/widgets/appwindow/appwindow.go
@@ -9,6 +9,7 @@ func SetValues(w gui.GUI) gui.GUI {
 	// AppWindow
 	w.AppWindow.Add(w.TrackBox)
 	w.AppWindow.SetTitlebar(w.Header)
+	w.AppWindow.SetDefaultSize(gui.ArtSize, gui.ArtSize)
 	w.AppWindow.Show()
 	return w
 }
