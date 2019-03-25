21a0f88b4b4caed7591a29da6a81decb6950c7e1
Octopus Octopus <oct2pus@jade.moe>
2019-03-24 22:48:48 -0500

    find cover.imgext if no id3 art exists, otherwise fail but dont crash

--- logic/art.go
+++ logic/art.go
@@ -1,47 +1,87 @@
 package logic
 
 import (
-	//	"github.com/dhowden/tag"
-
 	"errors"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/gotk3/gotk3/gdk"
 )
 
-// FindArt takes the art from the track and returns a pixbuf of it
+// FindArt takes the art from the track and returns a pixbuf of it.
 func FindArt(a AudioData) (*gdk.Pixbuf, error) {
-
 	// process picture
 	mus, meta := a.openMusic()
 	defer mus.Close()
 
 	pic := meta.Picture()
 
-	// pic doesn't return an error, it only nils
-	if pic == nil {
-		return nil, errors.New("No picture found")
-	}
-
+	// todo (possibly maybe) wrap pixbuf_new_from_bytes for gotk3
 	f, err := os.Create("./.temp_cover")
 	if err != nil {
 		SendError(err, "creating file")
 	}
 	defer f.Close()
+
 	defer os.Remove("./.temp_cover")
 
-	_, err = f.Write((*pic).Data)
-	if err != nil {
-		SendError(err, "writing to cover")
+	// if we don't have a picture, look for cover.ext
+	if pic == nil {
+		cf, err := readFromCoverImg(mus)
+		if err != nil {
+			SendError(err, "no cover image, ignoring")
+			return nil, err
+		}
+		defer cf.Close()
+		fileInfo, err := cf.Stat()
+		if err != nil {
+			SendError(err, "could not read cover fileInfo, ignoring")
+			return nil, err
+		}
+		coverData := make([]byte, fileInfo.Size())
+		_, err = cf.Read(coverData)
+		if err != nil {
+			SendError(err, "reading from cover.ext")
+		}
+		f.Write(coverData)
+	} else {
+		_, err = f.Write((*pic).Data)
+		if err != nil {
+			SendError(err, "writing to cover")
+		}
 	}
 
+	// this is done because i can't load images directly into pixbuf
+	// from a byte stream
 	pix, err := gdk.PixbufNewFromFileAtScale("./.temp_cover",
 		ArtSize,
 		ArtSize,
 		true)
+
 	if err != nil {
 		SendError(err, "setting album art pixbuf")
 	}
 
 	return pix, nil
+}
+
+func readFromCoverImg(mus *os.File) (*os.File, error) {
+	// formats supported by gdk.Pixbuf
+	imgFormats := []string{".png", ".jpeg", ".jpg", ".tiff", ".tga", ".gif"}
+	dir, _ := path.Split(mus.Name())
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, errors.New("Woah baby")
+	}
+	for _, file := range files {
+		for _, format := range imgFormats {
+			if strings.Contains(file.Name(), format) {
+				return os.Open(dir + file.Name())
+			}
+		}
+	}
+
+	return nil, errors.New("No cover image found")
 }
